322ec5d2aa45dee9ca1be2430c84c059a8c19278
Andrew Park <apark999@gmail.com>
2021-02-24 03:09:48 -0800

    server js added

---
+++ server.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/database1',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
