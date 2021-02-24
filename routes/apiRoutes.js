88fb52a0d793d553df6dc4b686a42e417f8cbf8a
Andrew Park <apark999@gmail.com>
2021-02-24 15:58:24 -0800

    mostly functional

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,26 +1,29 @@
 const db = require('../models');
-const router = require('express').Router();
+const apiRouter = require('express').Router();
 
-// get workouts
-/*
-router.get('/api/workouts', (req, res) => {
+// get workouts and its total duration
+apiRouter.get('/api/workouts', (req, res) => {
   db.Workout.aggregate([
     {
-
+      $addFields: {
+        totalDuration: { $sum: '$exercises.duration' }
+      }
     }
   
   ])
   .then(dbWorkout => {
+    console.log(`Result of agregate: ${dbWorkout}`);
     res.json(dbWorkout);
   })
   .catch(err => {
     res.status(400).json(err);
   });
 
-}); */
+});
 
 // create workout
-router.post('/api/workouts', ({ body }, res) => {
+apiRouter.post('/api/workouts', ({ body }, res) => {
+  //console.log(req.body);
   db.Workout.create({ body })
     .then(dbWorkout => {
       res.json(dbWorkout);
@@ -30,9 +33,41 @@ router.post('/api/workouts', ({ body }, res) => {
     });
 });
 
-module.exports = router;
+// add an exercise on a given workout
+apiRouter.put('/api/workouts/:id', (req, res) => {
+  console.log(req.body);
+  db.Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: {exercises: req.body} },
+    { new: true })
+    .then(dbWorkout => {
+      console.log(dbWorkout);
+      res.json(dbWorkout);
+    }).catch(err => {
+      res.status(400).json(err);
+    });
+  
+});
 
-// add an exercise to workout
+// get combined weight from the past seven workouts
+// get total duration of each workout from past seven workouts
+apiRouter.get('/api/workouts/range', (req, res) => {
+  db.Workout.aggregate([
+    {
+      $addFields: {
+        totalDuration: { $sum: '$exercises.duration'},
+        combinedWeight: { $sum: '$exercises.weight'}
+      }
+    },
+    { $sort: { day: -1 } },
+    { $limit: 7 }
+  ])
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
 
-
-// get
+module.exports = apiRouter;
